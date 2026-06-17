89022aff6d14ad82ae1e8341833284fd97bdba70
116323-f <116323@selwyn.school.nz>
2026-06-17 14:54:21 +1200

    X key slightly works using old input system

--- Assets/Script/OnMouse.cs
+++ Assets/Script/OnMouse.cs
@@ -8,13 +8,16 @@ public class OnMouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public void OnPointerEnter(PointerEventData eventData)
     {
         print($"On Mouse Enter On {this.name}!");
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            print("X key is down");
+
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         print($"On Mouse Exit On {this.name}!");
     }
-
-    
-
 }
