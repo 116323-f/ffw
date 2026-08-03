b79233609cf825b87bce13cde04a85d7ee69111d
116323-f <116323@selwyn.school.nz>
2026-08-04 09:32:40 +1200

    Connected UI to scenes

---
+++ Assets/Script/LevelSelection.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
