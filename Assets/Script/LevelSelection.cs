9af6b0072bca52e70e0f0b8d141faa2ceffb4909
116323-f <116323@selwyn.school.nz>
2026-08-12 14:40:04 +1200

    Added auto button array

--- Assets/Script/LevelSelection.cs
+++ Assets/Script/LevelSelection.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 public class LevelSelection : MonoBehaviour
 {
     public Button[] buttons;
+    public GameObject levelButtons;
 
     private void Awake()
     {
+        //run ButtonsToArray first
+        ButtonsToArray();
         //first level unlocked when game is first launched
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
         //for loop to disable interactable property of all buttons
@@ -34,9 +37,23 @@ public class LevelSelection : MonoBehaviour
         SceneManager.LoadSceneAsync(2);
     }
 
+    //return to main menu
     public void MainMenu()
     {
         SceneManager.LoadSceneAsync(0);
+    }
+
+    void ButtonsToArray()
+    {
+        //set size of button array equal to number of child objects of levelButtons object
+        int childCount = levelButtons.transform.childCount;
+        buttons = new Button [childCount];
+        //assign button component of levelButtons object's children to the buttons array in order
+        for (int i = 0; i < childCount; i++)
+        {
+            buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
+
     }
 
 }
